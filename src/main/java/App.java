31d634e2c9a0e995b61bc3f96e597ae9a0b5e335
Pawel Cwik <pawel@clockworkjava.pl>
2020-06-08 21:41:24 +0200

    Moduł 1 - Zmienne

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,16 @@ public class App {
     public static void main(String[] args) {
 
         /* To jest wiadomość powitalna */
-        System.out.println("Witam w systemie rezerwacji dla hotelu Overlook");
+        String nazwaHotelu = "Overlook";
+        int wersjaSystemu = 1;
+        boolean wersjaDeveloperska = true;
+
+
+        System.out.print("Witam w systemie rezerwacji dla hotelu");
+        System.out.println(nazwaHotelu);
         System.out.println("Aktualna wersja systemu: ");
-        System.out.println(1);
+        System.out.println(wersjaSystemu);
         System.out.println("Wersja developerska: ");
-        System.out.println(true);
+        System.out.println(wersjaDeveloperska);
     }
 }
