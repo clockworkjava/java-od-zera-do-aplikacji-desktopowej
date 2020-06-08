8abc6777ac9623e51f2cecbe5c79d6d2f1fc868f
Pawel Cwik <pawel@clockworkjava.pl>
2020-06-08 21:37:31 +0200

    Moduł 1 - Podstawowe typy danych

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Witam w systemie rezerwacji dla hotelu Overlook");
+        System.out.println("Aktualna wersja systemu: ");
+        System.out.println(1);
+        System.out.println("Wersja developerska: ");
+        System.out.println(true);
     }
 }
