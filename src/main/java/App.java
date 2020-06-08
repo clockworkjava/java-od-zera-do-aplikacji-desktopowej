c70de966afa2faf58bd7c7c7fe4eb530ec00fb88
Pawel Cwik <pawel@clockworkjava.pl>
2020-06-08 21:45:27 +0200

    Moduł 1 - Klasy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,16 +3,18 @@ public class App {
     public static void main(String[] args) {
 
         /* To jest wiadomość powitalna */
-        String nazwaHotelu = "Overlook";
-        int wersjaSystemu = 1;
-        boolean wersjaDeveloperska = true;
+        String hotelName = "Overlook";
+        int systemVersion = 1;
+        boolean isDeveloperVersion = true;
 
 
         System.out.print("Witam w systemie rezerwacji dla hotelu");
-        System.out.println(nazwaHotelu);
+        System.out.println(hotelName);
         System.out.println("Aktualna wersja systemu: ");
-        System.out.println(wersjaSystemu);
+        System.out.println(systemVersion);
         System.out.println("Wersja developerska: ");
-        System.out.println(wersjaDeveloperska);
+        System.out.println(isDeveloperVersion);
+
+        Person pawel = new Person();
     }
 }
