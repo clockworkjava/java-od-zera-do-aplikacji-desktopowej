c01b74eb6ca8715b2dac55bcc445f1513792018e
Pawel Cwik <pawel@clockworkjava.pl>
2020-06-11 15:34:13 +0200

    Moduł 7 - Pola obiektu

--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -1,6 +1,19 @@
 public class Guest {
 
+    String firstName;
+    String lastName;
+    int age;
+
     public Guest() {
         System.out.println("Stworzono nowy obiekt klasy Guest");
+    }
+
+    public Guest(String newFirstName, String newLastName, int newAge) {
+        firstName = newFirstName;
+        lastName = newLastName;
+        age = newAge;
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(age);
     }
 }
