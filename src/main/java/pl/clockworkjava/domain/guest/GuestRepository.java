8deec4df2625304bfb7f151d18d57820d155b423
Pawel Cwik <pawel@clockworkjava.pl>
2020-07-02 23:12:22 +0200

    Moduł 18 - lista

--- src/main/java/pl/clockworkjava/domain/guest/GuestRepository.java
+++ src/main/java/pl/clockworkjava/domain/guest/GuestRepository.java
@@ -1,6 +1,11 @@
 package pl.clockworkjava.domain.guest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GuestRepository {
+
+    List<Guest> guests = new ArrayList<>();
 
     Guest createNewGuest(String firstName, String lastName, int age, Gender gender) {
         return new Guest(firstName, lastName, age, gender);
