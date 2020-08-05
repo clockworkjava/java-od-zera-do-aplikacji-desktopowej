da63654741eb7b1f7b4dae699409d659729d64d1
Pawel Cwik <pawel@clockworkjava.pl>
2020-08-05 18:30:36 +0200

    Moduł 22 - Hello JavaFX

--- src/main/java/pl/clockworkjava/App.java
+++ src/main/java/pl/clockworkjava/App.java
@@ -1,12 +1,16 @@
 package pl.clockworkjava;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import pl.clockworkjava.exceptions.PersistenceToFileException;
 import pl.clockworkjava.ui.text.TextUI;
 import pl.clockworkjava.util.Properties;
 
 import java.io.IOException;
 
-public class App {
+public class App extends Application {
 
     private static final TextUI textUI = new TextUI();
 
@@ -17,7 +21,19 @@ public class App {
         } catch (IOException e) {
             throw new PersistenceToFileException(Properties.DATA_DIRECTORY.toString(), "create", "directory");
         }
-        textUI.showSystemInfo();
-        textUI.showMainMenu();
+        Application.launch(args);
+//        textUI.showSystemInfo();
+//        textUI.showMainMenu();
+    }
+
+    public void start(Stage primaryStage) {
+        String hotelName = Properties.HOTEL_NAME;
+        int systemVersion = Properties.SYSTEM_VERSION;
+        Label l = new Label("Hello JavaFX!");
+        Scene scene = new Scene(l, 640, 480);
+        String title = String.format("System rezerwacji hotelu %s (%d)", hotelName, systemVersion);
+        primaryStage.setTitle(title);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
