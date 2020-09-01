8de8d40a5bdc980c94eb1e0d69a5579c261aa9fc
Pawel Cwik <pawel@clockworkjava.pl>
2020-09-01 16:16:50 +0200

    Moduł 27 - GridPane

--- src/main/java/pl/clockworkjava/ui/gui/AddNewRoomScene.java
+++ src/main/java/pl/clockworkjava/ui/gui/AddNewRoomScene.java
@@ -1,10 +1,12 @@
 package pl.clockworkjava.ui.gui;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -23,28 +25,30 @@ public class AddNewRoomScene {
 
     public AddNewRoomScene(Stage stg, TableView<RoomDTO> tableView) {
 
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setVgap(20);
+
         Label roomNumberLabel = new Label("Numer pokoju:");
         TextField roomNumberField = new TextField();
-        HBox roomNumber = new HBox(roomNumberLabel, roomNumberField);
+
+        gridPane.add(roomNumberLabel, 0, 0);
+        gridPane.add(roomNumberField, 1, 0);
 
         Label bedTypeLabel = new Label("Typy łóżek:");
 
         Button addNewBedButton = new Button();
-
         Image icon = new Image(getClass().getClassLoader().getResourceAsStream("add.jpg"));
-
         ImageView imgView = new ImageView(icon);
-
         imgView.setFitWidth(16);
         imgView.setFitHeight(16);
-
         addNewBedButton.setGraphic(imgView);
-
         addNewBedButton.setPadding(Insets.EMPTY);
 
-        HBox bedTypeRow = new HBox(bedTypeLabel, addNewBedButton);
+        gridPane.add(bedTypeLabel, 0, 1);
+        gridPane.add(addNewBedButton, 1, 1);
 
-        VBox bedsVerticalLayout = new VBox(bedTypeRow, getComboBox());
+        VBox bedsVerticalLayout = new VBox(getComboBox());
 
         addNewBedButton.setOnAction( actionEvent -> {
             bedsVerticalLayout.getChildren().add(getComboBox());
@@ -69,9 +73,14 @@ public class AddNewRoomScene {
             stg.close();
         });
 
-        VBox mainFormLayout = new VBox(roomNumber, bedsVerticalLayout, addNewRoomButton);
+        addNewBedButton.setPadding(new Insets(5,5,5,5));
 
-        this.mainScene = new Scene(mainFormLayout, 640, 480);
+        gridPane.add(bedsVerticalLayout, 1, 2);
+        gridPane.add(addNewRoomButton, 0, 3);
+
+
+
+        this.mainScene = new Scene(gridPane, 640, 480);
     }
 
     private ComboBox<String> getComboBox() {
