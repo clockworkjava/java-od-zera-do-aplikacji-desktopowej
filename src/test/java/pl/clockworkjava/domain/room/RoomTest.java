c1257ac4800a2c97f9504d813e0f8a278c140835
Pawel Cwik <pawel@clockworkjava.pl>
2020-09-28 14:52:47 +0200

    Moduł 32 - Struktura testu

--- src/test/java/pl/clockworkjava/domain/room/RoomTest.java
+++ src/test/java/pl/clockworkjava/domain/room/RoomTest.java
@@ -14,40 +14,46 @@ public class RoomTest {
     @Test
     public void toCSVTest() {
 
+        //given
         List<BedType> beds = Arrays.asList(BedType.values());
-
         Room room = new Room(1, 302, beds);
 
-        String csvTemplate = "1,302,Pojedyncze#Podwójne#Królewskie"+System.getProperty("line.separator");
-
+        //when
         String createdCSV = room.toCSV();
 
+        //then
+        String csvTemplate = "1,302,Pojedyncze#Podwójne#Królewskie"+System.getProperty("line.separator");
         assertEquals(csvTemplate, createdCSV, "Porównanie wygenerowanych formatów CSV");
 
     }
 
     @Test
     public void toCSVWithNullBedListTest() {
+
+        //given
         Room room = new Room(1, 302, null);
 
+        //when
+        String createdCSV = room.toCSV();
+
+        //then
         assertNotNull(room.getBeds());
 
         String csvTemplate = "1,302,"+System.getProperty("line.separator");
-
-        String createdCSV = room.toCSV();
-
         assertEquals(csvTemplate, createdCSV, "Porównanie wygenerowanych formatów CSV przy liście łózek == null");
     }
 
     @Test
     public void toDTOTest() {
 
+        //given
         List<BedType> beds = Arrays.asList(BedType.values());
-
         Room room = new Room(1, 302, beds);
 
+        //when
         RoomDTO roomDTO = room.generateDTO();
 
+        //then
         assertEquals(roomDTO.getId(),1);
         assertEquals(roomDTO.getNumber(), 302);
         assertEquals(roomDTO.getBedsCount(), 3);
@@ -58,10 +64,13 @@ public class RoomTest {
     @Test
     public void toDTOFromRoomWithNullBedsListTest() {
 
+        //given
         Room room = new Room(1, 302, null);
 
+        //when
         RoomDTO roomDTO = room.generateDTO();
 
+        //then
         assertEquals(roomDTO.getId(),1);
         assertEquals(roomDTO.getNumber(), 302);
         assertEquals(roomDTO.getBedsCount(), 0);
